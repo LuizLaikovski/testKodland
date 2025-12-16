89148f895e863a1ea8ce49134a2ec30896c4c4ca
Luiz Laikovski <luizlaikovski@gmail.com>
2025-12-16 11:28:37 -0300

    Arrumando o game

---
+++ sprites_person.py
@@ -0,0 +1,22 @@
+# Sprites de caminhada
+walk_sprites_person_right = [
+    'p2_walk01',
+    'p2_walk02',
+    'p2_walk03',
+    'p2_walk04',
+    'p2_walk05'
+]
+
+walk_sprites_person_left = [
+    'p2_walk01_left',
+    'p2_walk02_left',
+    'p2_walk03_left',
+    'p2_walk04_left',
+    'p2_walk05_left'
+]
+
+jumpingsprite = 'p2_jump'
+jumpingsprite_left = 'p2_jump_left'
+
+SPRITE_HURT = 'p2_hurt'
+SPRITE_NORMAL = 'p2_front'
